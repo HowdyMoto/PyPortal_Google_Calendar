3f4abd2f935a401b6577510a5580781226ac225d
Wright Bagwell <43309236+HowdyMoto@users.noreply.github.com>
2022-09-20 09:26:01 -0700

    Fixed typo in secrets.py

--- secrets.py
+++ secrets.py
@@ -5,8 +5,8 @@ secrets = {
     'ssid' : 'YOUR_SSID',
     'password' : 'YOUR_SSID_PASS',
     'aio_username': 'YOUR_AIO_USERNAME',
-    'aio_key': 'YOUR_AIO_KEY'
-    'timezone' : "Etc/UTC", # http://worldtimeapi.org/timezones
+    'aio_key': 'YOUR_AIO_KEY',
+    'timezone': "Etc/UTC", # http://worldtimeapi.org/timezones
     'google_client_id' : 'YOUR_GOOGLE_CLIENT_ID',
     'google_client_secret' : 'YOUR_GOOGLE_CLIENT_SECRET',
     'google_email' : "YOUR_GOOGLE_EMAIL",
