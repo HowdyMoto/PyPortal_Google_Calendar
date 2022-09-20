6fbd6fb8d3ff88724411d6454a920b538a10dd64
Wright Bagwell <43309236+HowdyMoto@users.noreply.github.com>
2022-09-20 10:34:29 -0700

    Added comments and placeholder token blocks

--- secrets.py
+++ secrets.py
@@ -4,10 +4,12 @@
 secrets = {
     'ssid' : 'YOUR_SSID',
     'password' : 'YOUR_SSID_PASS',
-    'aio_username': 'YOUR_AIO_USERNAME',
-    'aio_key': 'YOUR_AIO_KEY',
-    'timezone': "Etc/UTC", # http://worldtimeapi.org/timezones
-    'google_client_id' : 'YOUR_GOOGLE_CLIENT_ID',
-    'google_client_secret' : 'YOUR_GOOGLE_CLIENT_SECRET',
-    'google_email' : "YOUR_GOOGLE_EMAIL",
+    'aio_username': 'YOUR_AIO_USERNAME',                    # your adafruit IO account username
+    'aio_key': 'YOUR_AIO_KEY',                              # from your adafruit IO account
+    'timezone': "Etc/UTC",                                  # http://worldtimeapi.org/timezones
+    'google_client_id' : 'YOUR_GOOGLE_CLIENT_ID',           # From Google cloud app dashbaord
+    'google_client_secret' : 'YOUR_GOOGLE_CLIENT_SECRET',   # From Google cloud app dashbaord
+    'google_email' : "YOUR_GOOGLE_EMAIL",                   # your email used to log into your calendar/gmail
+    'google_access_token' : 'insert_here',                  # you'll get this after you use the authenticator
+    'google_refresh_token' : 'insert_here',                 # you'll get this after you use the authenticator
     }
