31bbfe6c67a7e0f038680b62d790bd620634f9e1
Wright Bagwell <43309236+HowdyMoto@users.noreply.github.com>
2023-01-19 19:13:06 -0800

    Update secrets.py
    Added missing keys to fill in

--- secrets.py
+++ secrets.py
@@ -4,12 +4,16 @@
 secrets = {
     'ssid' : 'YOUR_SSID',
     'password' : 'YOUR_SSID_PASS',
+
     'aio_username': 'YOUR_AIO_USERNAME',                    # your adafruit IO account username
     'aio_key': 'YOUR_AIO_KEY',                              # from your adafruit IO account
     'timezone': "Etc/UTC",                                  # http://worldtimeapi.org/timezones
+    'timezone_offset' : "-08:00",                           # Hour offset from UTC. I could calculate it, but it's much easier to just specify it here.
+
     'google_client_id' : 'YOUR_GOOGLE_CLIENT_ID',           # From Google cloud app dashbaord
     'google_client_secret' : 'YOUR_GOOGLE_CLIENT_SECRET',   # From Google cloud app dashbaord
     'google_email' : "YOUR_GOOGLE_EMAIL",                   # your email used to log into your calendar/gmail
     'google_access_token' : 'insert_here',                  # you'll get this after you use the authenticator
     'google_refresh_token' : 'insert_here',                 # you'll get this after you use the authenticator
+    
     }
